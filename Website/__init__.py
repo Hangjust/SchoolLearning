e491ebc3d81fac765e8e00ae95e37cf30a7c4548
Hangjust <giolutidze1@gmail.com>
2022-02-15 22:17:28 +0100

    ok

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
-from dotenv import load_dotenv
-import os
-load_dotenv()
+
+
 
 
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.getenv("API_KEY")
+    app.config['SECRET_KEY'] = 'iasifjj23iiUIQJEFIhij32o9d'
 
     from .views import views
     from .auth import auth
